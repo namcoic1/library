d22d08b36e201b8f09809c626c6649d71406c6ba
Nguyen Hoai Nam (K15 HL) <namnhhe151296@fpt.edu.vn>
2022-07-22 00:46:33 +0700

    Upload common feature

---
+++ LMS_Project/Logics/AuthorLogics.cs
@@ -0,0 +1,26 @@
+﻿using LMS_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMS_Project.Logics
+{
+    public class AuthorLogics
+    {
+        LMSContext db;
+
+        public AuthorLogics()
+        {
+            db = new LMSContext();
+        }
+        public List<Author> GetAllAut()
+        {
+            return db.Authors.ToList();
+        }
+        public List<Book> GetAllBByAutId(int autid)
+        {
+            return db.Books.Where(b => b.AutId == autid).ToList();
+        }
+    }
+}
---
+++ LMS_Project/Logics/UserLogics.cs
@@ -0,0 +1,33 @@
+﻿using LMS_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMS_Project.Logics
+{
+    public class UserLogics
+    {
+        LMSContext db;
+
+        public UserLogics()
+        {
+            db = new LMSContext();
+        }
+        public User GetUserLog(string email, string pass)
+        {
+            return db.Users.FirstOrDefault(u => u.UEmail.Equals(email) && u.UPassword.Equals(pass));
+        }
+        public User GetUserReg(string email)
+        {
+            return db.Users.FirstOrDefault(u => u.UEmail.Equals(email));
+        }
+        public void AddUser(User u)
+        {
+            u.UId = 0;
+            db.Users.Add(u);
+            db.SaveChanges();
+        }
+
+    }
+}
